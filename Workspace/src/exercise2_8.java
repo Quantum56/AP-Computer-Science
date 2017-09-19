ba8cdbdd3892f6918241884aaec1830433749313
Quantum56 <Quantum56@users.noreply.github.com>
2017-09-19 13:31:21 -0400

    Add files via upload

---
+++ Workspace/src/exercise2_8.java
@@ -0,0 +1,52 @@
+
+/**
+ * @author student
+ *
+ */
+import java.util.Scanner;
+
+public class exercise2_8 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*	int x = 2;
+			int x2 = 5;
+			int y = 7;
+			int y2 = 4; */
+		String message;
+	    Scanner sc = new Scanner(System.in);
+	    
+	    message = sc.nextLine();
+	    System.out.print("Enter point coordinates:");
+	    
+	    int x1;
+	    System.out.print("Enter x1: ");
+	    x1 = sc.nextInt();
+	    
+	    int x2;
+	    System.out.print("Enter x2: ");
+	    x2 = sc.nextInt();
+	    
+	    int y1;
+	    System.out.print("Enter y1: ");
+	    y1 = sc.nextInt();
+	    
+	    int y2;
+	    System.out.print("Enter y2: ");
+	    y2 = sc.nextInt();
+	    
+	    
+	   //  int x = sc.nextInt();
+	   //  int x2 = sc.nextInt();
+	   //  int y = sc.nextInt();
+	   //  int y2 = sc.nextInt();
+	     
+	     
+		double z = Math.sqrt(((x1 - x2) * (x1 - x2)) + ((y1 - y2) * (y1 - y2)));
+		System.out.printf("The distance between the first point and the second is " + z);
+	}
+
+}
