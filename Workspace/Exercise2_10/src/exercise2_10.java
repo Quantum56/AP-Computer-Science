569d197d3a90638047256b45624820b691e553da
Quantum56 <Quantum56@users.noreply.github.com>
2017-09-20 10:24:21 -0400

    Add files via upload

--- Workspace/Exercise2_10/src/exercise2_10.java
+++ Workspace/Exercise2_10/src/exercise2_10.java
@@ -19,7 +19,7 @@ public class exercise2_10 {
 		sc = new Scanner(System.in);
 
 		System.out.println("Heron's Formula Calculator - Press enter to initialize");
-		message = sc.nextLine();
+		setMessage(sc.nextLine());
 
 		double a;
 		System.out.println("Enter first length: ");
@@ -39,6 +39,14 @@ public class exercise2_10 {
 		double z = Math.sqrt(s * (s - a) * (s - b) * (s - c));
 		Math.round((z * 1.000) / 1.000); // attempt to round to 3 decimals
 		System.out.printf("The distance between the first point and the second is " + z);
+	}
+
+	public static String getMessage() {
+		return message;
+	}
+
+	public static void setMessage(String message) {
+		exercise2_10.message = message;
 	}
 
 }
