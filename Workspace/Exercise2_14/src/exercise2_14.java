23907dd22485b35e8df56a0df4df62c0845bf8cf
Quantum56 <Quantum56@users.noreply.github.com>
2017-09-21 19:39:50 -0400

    Add files via upload

--- Workspace/Exercise2_14/src/exercise2_14.java
+++ Workspace/Exercise2_14/src/exercise2_14.java
@@ -14,7 +14,6 @@ public class exercise2_14 {
 	/**
 	 * @param args
 	 */
-	String message;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Random generator = new Random();
@@ -23,18 +22,26 @@ public class exercise2_14 {
 		a = generator.nextInt(8) + 0;
 		b = generator.nextInt(8) + 0;
 		c = generator.nextInt(8) + 0;
-		d = generator.nextInt(8) + 0;
-		e = generator.nextInt(10) + 0;
-		f = generator.nextInt(10) + 0;
-		g = ((d * 100) + (e * 10 + f));
-		check:
-			if(g >= 742) {
+		// start:
+		do {
+			d = generator.nextInt(8) + 0;
+			e = generator.nextInt(10) + 0;
+			f = generator.nextInt(10) + 0;
+			g = ((d * 100) + (e * 10) + f);
+			final int v = g;
+			// check:
+			if (/* v < 0 || v > 742 */ v >= 742) {
 				System.out.printf("Incorrect Generation\n" + g);
-				d = d - 1;
-				break check;
+				// break start;
+				continue;
+				// break check;
 			}
-		z = generator.nextInt(9999) + 0001;
-		System.out.printf("Telephone " + a + b + c + "-" + d + e + f + "-" + z);
+		} while (false);
+		{
+			z = generator.nextInt(9999) + 0001;
+			System.out.printf("Telephone " + a + b + c + "-" + d + e + f + "-" + z);
+		}
+
 	}
 
 }
