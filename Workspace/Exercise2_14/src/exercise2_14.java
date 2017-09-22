1ab7927a4320e9f0caa23ab6c518808bef77ec81
Quantum56 <Quantum56@users.noreply.github.com>
2017-09-22 12:26:51 -0400

    Add files via upload

--- Workspace/Exercise2_14/src/exercise2_14.java
+++ Workspace/Exercise2_14/src/exercise2_14.java
@@ -1,6 +1,7 @@
 
 /**
  *  A simple random phone number generator with certain restrictions.
+ *  Comments are from debugged code that had to be left out, but shows the attempts to limit 742. 
  */
 
 import java.util.Random;
@@ -17,8 +18,7 @@ public class exercise2_14 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Random generator = new Random();
-		int a, b, c, d, e, f, g, z;
-		// x = generator.nextInt(999) + 001; // limitation on 9 and 8
+		int a, b, c, d, e, f, g, h, i, j, k, z;
 		a = generator.nextInt(8) + 0;
 		b = generator.nextInt(8) + 0;
 		c = generator.nextInt(8) + 0;
@@ -31,17 +31,20 @@ public class exercise2_14 {
 			final int v = g;
 			// check:
 			if (/* v < 0 || v > 742 */ v >= 742) {
-				System.out.printf("Incorrect Generation\n" + g);
-				// break start;
+				// System.out.printf("Incorrect Generation\n" + g);
+				d = d - 1;
 				continue;
 				// break check;
 			}
 		} while (false);
 		{
-			z = generator.nextInt(9999) + 0001;
+			h = generator.nextInt(10) + 0;
+			i = generator.nextInt(10) + 0;
+			j = generator.nextInt(10) + 0;
+			k = generator.nextInt(10) + 0;
+			z = ((h * 1000) + (i * 100) + (j * 10) + k);
 			System.out.printf("Telephone " + a + b + c + "-" + d + e + f + "-" + z);
 		}
-
 	}
 
 }
