8c56d29ce4b9481c8e8c5dd433342fb52701cbd3
Quantum56 <Quantum56@users.noreply.github.com>
2017-09-25 12:33:18 -0400

    Create exercise3_6.java

---
+++ Workspace/Exercise3_6/exercise3_6.java
@@ -0,0 +1,30 @@
+/**
+ * Work on an integer parser that reads number of odd, even and 0 digits.
+ */
+
+/**
+ * @author ZackB
+ *
+ */
+import java.util.Scanner;
+
+public class exercise3_6 {
+
+	private static Scanner sc;
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		sc = new Scanner(System.in);
+		int count;
+		System.out.printf("Enter an integer (enter 0 to quit): \n");
+		count = sc.nextInt();
+		final int LIMIT = count;
+		for (count = 1; count <= LIMIT; count++) {
+			System.out.println(count);
+			// The program currently reads each number from 0 to its limit
+		}
+	}
+
+}
