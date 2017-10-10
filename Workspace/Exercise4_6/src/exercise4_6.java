ef4e5b012dea838dedef103eb35f26f489a8f0c1
Quantum56 <Quantum56@users.noreply.github.com>
2017-10-10 14:28:03 -0400

    Add files via upload

---
+++ Workspace/Exercise4_6/src/exercise4_6.java
@@ -0,0 +1,87 @@
+import java.util.Random;
+
+/**
+ *  Application that prints 20 random cards ace, 2, 3, 4, 5, 6, 7, 8, 9, 10, jack, queen, king
+ */
+
+/**
+ * @author ZackB
+ *
+ */
+public class exercise4_6 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Random gen = new Random();
+		final int x = 20;
+		int a;
+		for (int i = 0; i < x; i++) {
+			a = gen.nextInt(13) + 1;
+			int c = gen.nextInt(4) + 1;
+			String str = "";
+			String d = "";
+			if (a > 2 && a <= 10) {
+				if (c == 1) {
+					d = "Spades";
+				}
+				if (c == 2) {
+					d = "Clubs";
+				}
+				if (c == 3) {
+					d = "Hearts";
+				}
+				if (c == 4) {
+					d = "Diamonds";
+				}
+				System.out.print(a);
+				System.out.print(" of ");
+				System.out.println(d);
+				// System.out.printf(str, " of ", d);
+			} else {
+				if (c == 1) {
+					d = "Spades";
+				}
+				if (c == 2) {
+					d = "Clubs";
+				}
+				if (c == 3) {
+					d = "Hearts";
+				}
+				if (c == 4) {
+					d = "Diamonds";
+				}
+				if (a <= 2 && a >= 10) {
+					if (c == 1) {
+						d = "Spades";
+					} else if (c == 2) {
+						d = "Clubs";
+					} else if (c == 3) {
+						d = "Hearts";
+					} else if (c == 4) {
+						d = "Diamonds";
+					}
+					if (a == 1) {
+						str = "Ace";
+						// System.out.print(str);
+					} else if (a == 11) {
+						str = "Jack";
+						// System.out.print(str);
+					} else if (a == 12) {
+						str = "Queen";
+						// System.out.print(str);
+					} else if (a == 13) {
+						str = "King";
+						// System.out.print(str);
+					}
+				}
+				System.out.print(str);
+				System.out.print(" of ");
+				System.out.println(d);
+		}
+
+	}
+	}
+}
