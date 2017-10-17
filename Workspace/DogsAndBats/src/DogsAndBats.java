f6037d78e264e77d31e7b0b68d4bfe816019e572
Quantum56 <Quantum56@users.noreply.github.com>
2017-10-17 13:30:49 -0400

    Add files via upload

--- Workspace/DogsAndBats/src/DogsAndBats.java
+++ Workspace/DogsAndBats/src/DogsAndBats.java
@@ -23,9 +23,9 @@ public class DogsAndBats {
 		int numLegs = bat1NumLegs + 4;
 		double totalWeight = bat1Weight + dog1Weight;
 		System.out.println(dog1Name + " is " + dog1Weight + " LBS and is " + dog1Color + ".");
-		System.out.println(bat1Name + " is " + bat1Weight + " LBS and is " + bat1Color + ".");
-		System.out.println("There are " + numLegs + " total legs with " + dog1Name + " and " + bat1Name + " weighing " + totalWeight + " LBS.");
-		
+		System.out.println(bat1Name + " is " + bat1Weight + " LBS and is " + bat1Color + ", and has " + bat1NumLegs + " legs.");
+		System.out.println("There are " + numLegs + " total legs with " + dog1Name + " and " + bat1Name + " weighing "+ totalWeight + " LBS.");
+
 	}
 
 }
