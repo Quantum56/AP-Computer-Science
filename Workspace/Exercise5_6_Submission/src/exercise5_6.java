afdd87edbb2eca498f418186aa277cae1ab453db
Quantum56 <Quantum56@users.noreply.github.com>
2017-10-27 13:57:53 -0400

    Update exercise5_6.java

--- Workspace/Exercise5_6_Submission/src/exercise5_6.java
+++ Workspace/Exercise5_6_Submission/src/exercise5_6.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * @author ZackB
  *
@@ -5,15 +7,29 @@
 public class exercise5_6 {
 
 	private static Coin a;
+	private static int b;
+	private static Scanner sc = new Scanner(System.in);
 
 	/**
 	 * @param args
-	 * @throws Exception 
+	 * @throws Exception
 	 */
 	public static void main(String[] args) throws Exception {
 		a = new Coin();
-		a.flip();
-		System.out.println("Coin is: " + a);
+		System.out.println("Set key: ");
+		b = sc.nextInt();
+		a.setKey(b);
+		a.unlock(b);
+		System.out.println("Input number of flips: ");
+		for (int i = 0; i < b; i++) {
+			System.out.println("Input code to unlock: ");
+			a.unlock(sc.nextInt());
+			a.flip();
+			System.out.println("Coin is: " + a);
+			System.out.println("Input code to lock: ");
+			a.lock(sc.nextInt());
+		}
+		a.lock(b);
 	}
 
 }
