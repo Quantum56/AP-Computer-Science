4749b73f5655e6c9d9c2c6c729d6dd0c59ce56a3
Quantum56 <Quantum56@users.noreply.github.com>
2017-10-31 13:12:53 -0400

    Commented out unnecessary lines

--- Workspace/Cards/src/DeckShuffle.java
+++ Workspace/Cards/src/DeckShuffle.java
@@ -1,6 +1,6 @@
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+// import java.util.Arrays;									//unnecessary
+// import java.util.HashMap;									//unnecessary
+// import java.util.Map;									//unnecessary
 
 /**
  * @author ZackB
@@ -8,8 +8,8 @@ import java.util.Map;
 
 public class DeckShuffle {
 
-	private static String finalResult;
-	private static String sevenOfHearts = "7 of Hearts";
+//	private static String finalResult;							//unnecessary
+//	private static String sevenOfHearts = "7 of Hearts";					//unnecessary
 
 	/**
 	 * @param args
@@ -22,7 +22,6 @@ public class DeckShuffle {
 		for (int i = 0; i < deck.length; i++) {
 			deck[i] = i;
 		}
-
 		for (int i = 0; i < deck.length; i++) {
 			int index = (int) (Math.random() * deck.length);
 			int temp = deck[i];
@@ -35,13 +34,13 @@ public class DeckShuffle {
 			String rank = cards[deck[i] % 13];
 			String result = rank + " of " + suit;
 			System.out.println(result);
-			Map<Integer, String> map = new HashMap<Integer, String>();
-			finalResult = map.put(Integer.valueOf(i), result);
+			// Map<Integer, String> map = new HashMap<Integer, String>();		//unnecessary
+			// finalResult = map.put(Integer.valueOf(i), result);			//unnecessary
 
 		}
-		Arrays.asList(finalResult).indexOf(sevenOfHearts);
-		finalResult = finalResult.replaceAll("null", "");
-		System.out.println(finalResult);
+		// Arrays.asList(finalResult).indexOf(sevenOfHearts);				//unnecessary
+		// finalResult = finalResult.replaceAll("null", "");				//unnecessary
+		// System.out.println(finalResult);						//unnecessary
 	}
 
 }
