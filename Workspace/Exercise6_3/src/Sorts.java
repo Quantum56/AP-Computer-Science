8c41e4a22164f5c68c3192b7ed8455fabf82e17a
Quantum56 <Quantum56@users.noreply.github.com>
2017-11-09 12:37:07 -0500

    Add files via upload

---
+++ Workspace/Exercise6_3/src/Sorts.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+import java.lang.System;
+/**
+ * @author student
+ *
+ */
+public class Sorts {
+
+	public static void selectionSort(int[] numbers) {
+		int min, temp;
+
+		for (int index = 0; index < numbers.length - 1; index++) {
+			min = index;
+			for (int scan = index + 1; scan < numbers.length; scan++) {
+				if (numbers[scan] < numbers[min]) {
+					min = scan;
+				}
+			}
+
+			temp = numbers[min];
+			numbers[min] = numbers[index];
+			numbers[index] = temp;
+		}
+		
+		numbers = arraycopy(numbers.length - 1, 0);
+		/*
+		for (int i = 0; i < numbers.length; i++) {
+			if (i < (numbers.length - 1) / 2) {
+			// numbers[i] = numbers[numbers.length - (i + 1)];
+			numbers[numbers.length - (i + 1)] = numbers[i];
+			}
+		}
+		*/
+		
+	/*	int a = 0;
+		for (int i = 0; i < numbers.length; i++) {
+			while (a > 0) {
+				if (10 - a > 0)
+					numbers[i] = numbers[10 - a];
+				a++;
+			}
+		}
+		*/
+		
+	}
+}
---
+++ Workspace/Exercise6_3/src/exercise6_3.java
@@ -0,0 +1,28 @@
+import java.util.Random;
+
+/**
+ * 
+ */
+
+/**
+ * @author ZackB
+ *
+ */
+public class exercise6_3 {
+	private static Random a = new Random();
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] sorting = {a.nextInt(100) + 1, a.nextInt(100) + 1, a.nextInt(100) + 1, a.nextInt(100) + 1, a.nextInt(100) + 1, a.nextInt(100) + 1, a.nextInt(100) + 1, a.nextInt(100) + 1, a.nextInt(100) + 1, a.nextInt(100) + 1, a.nextInt(100) + 1, };
+		Sorts.selectionSort(sorting);
+		
+		for (int i = 10; i < sorting.length; i--) {
+			if (i > 0) {
+			System.out.println(sorting[i] + "");		
+			}
+		}
+	}
+
+}
