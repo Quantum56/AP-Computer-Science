07b8b7cb09daf706cf5e6bf433f1427474fc95a3
Quantum56 <Quantum56@users.noreply.github.com>
2017-12-01 16:07:46 -0500

    Update Account.java

--- Workspace/Exercise6_6/src/Account.java
+++ Workspace/Exercise6_6/src/Account.java
@@ -1,11 +1,12 @@
-import java.text.NumberFormat;
+import java.text.DecimalFormat;
+// import java.text.NumberFormat;
 import java.util.ArrayList;
 
 public class Account {
-	private NumberFormat fmt = NumberFormat.getCurrencyInstance();
+	private DecimalFormat fmt = new DecimalFormat("#.##");
 
 	private final double RATE = 0.03;
-	private static double fee = 2.00;
+	private final double FEE = 2.00;
 
 	// private int[] acctNumbers = new int[30];
 	// private double[] balances = new double[30];
@@ -15,28 +16,32 @@ public class Account {
 	ArrayList<String> names = new ArrayList<String>(30);
 	ArrayList<Double> balances = new ArrayList<Double>(30);
 
-	public Account(String owner, double initial) {
+	public Account(String owner, double initial, int acctNum) {
+		acctNumbers.add(acctNum);
 		names.add(owner); // [account] = owner;
 		acctNumbers.add(names.indexOf(owner)); // [account] = account;
 		balances.add(names.indexOf(owner), initial); // [account] = initial;
+		// mainMenu();
 	}
 
-	public double deposit(double amount, String holdername) {
-		int account = names.indexOf(holdername);
-		if (amount < 0 || amount <= fee) {
+	// public static void mainMenu() {}
+
+	public double deposit(double amount, int acctNum) {
+		int account = acctNumbers.indexOf(acctNum);
+		if (amount < 0 || amount <= FEE) {
 			System.out.println("\n");
 			System.out.println("Error: Deposit amount is invalid.");
 			System.out.println(acctNumbers.get(account) + " " + fmt.format(amount));
 		} else {
 			balances.add(account, amount); // [account] = balances[account] + amount;
 		}
-		addInterest(holdername);
+		addInterest(acctNum);
 		return balances.get(account); // [account];
 	}
 
-	public double withdraw(double amount, String holdername) {
-		amount += fee;
-		int account = names.indexOf(holdername);
+	public double withdraw(double amount, int acctNum) {
+		amount += FEE;
+		int account = acctNumbers.indexOf(acctNum);
 
 		if (amount < 0) {
 			System.out.println("\n");
@@ -54,22 +59,30 @@ public class Account {
 				balances.add(account, -(amount)); // [account] = balances[account] - amount;
 			}
 		}
-		addInterest(holdername);
+		addInterest(acctNum);
+		if (balances.get(account) < 0) {
+			balances.add(account, Math.abs(balances.get(account)));
+		}
 		return balances.get(account); // [account];
 	}
 
-	public double addInterest(String holdername) {
-		int account = names.indexOf(holdername);
-		balances.add(account, (balances.get(account) * RATE)); // [account] += (balances[account] * RATE);
+	public double addInterest(int acctNum) {
+		int account = acctNumbers.indexOf(acctNum);
+		balances.add(account, (balances.get(account) * RATE) + balances.get(account)); // [account] +=
+																						// (balances[account] * RATE);
 		return balances.get(account); // [account];
 	}
 
-	public double getBalance(String holdername) {
-		int account = names.indexOf(holdername);
+	public double getBalance(int acctNum) {
+		int account = acctNumbers.indexOf(acctNum);
 		if (account != -1) {
 			return balances.get(account);
 		} else
 			return 0;
+	}
+
+	public String getAccountName(int acctNum) {
+		return (names.get(acctNumbers.indexOf(acctNum)));
 	}
 
 	public int getAccountNumber(String holderName) {
@@ -78,10 +91,11 @@ public class Account {
 		 * (names[i].equals(holderName)) { index = i; break; } }
 		 */
 		int a = names.indexOf(holderName);
-		return acctNumbers.get(a); // [index];
+		return acctNumbers.get(a) + 1; // [index];
 	}
 
-	public String toString(int account) {
+	public String toString(int acctNum) {
+		int account = acctNumbers.indexOf(acctNum);
 		return (acctNumbers.get(account) + "\t" + names.get(account) + "\t" + fmt.format(balances.get(account)));
 	}
 }
