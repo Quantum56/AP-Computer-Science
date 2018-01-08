0db7bf3eae3dfbbb5901bf37684bafa6ae340fdf
Quantum56 <Quantum56@users.noreply.github.com>
2018-01-08 13:01:38 -0500

    Add files via upload

--- Workspace/Exercise7_4/src/SportTrack.java
+++ Workspace/Exercise7_4/src/SportTrack.java
@@ -1,57 +1,323 @@
 import java.util.ArrayList;
 
 /**
- * 
- */
-
-/**
- * @author student
+ * @author ZackB
  *
  */
 public class SportTrack {
-	
+
 	public SportTrack() {
 		// TODO Auto-generated constructor stub
-		
 	}
-	
-	public class Football extends SportTrack {
+
+	public static class Football extends SportTrack {
 		private ArrayList<String> names = new ArrayList<String>();
 		private ArrayList<String> divisions = new ArrayList<String>();
 		private ArrayList<Integer> winsArr = new ArrayList<Integer>();
 		private ArrayList<Integer> lossesArr = new ArrayList<Integer>();
 		private ArrayList<Integer> pointsScoredArr = new ArrayList<Integer>();
+		private ArrayList<Integer> pointsScoredOnArr = new ArrayList<Integer>();
 		private ArrayList<Double> winLossPctArr = new ArrayList<Double>();
 		private ArrayList<Integer> pointDiffArr = new ArrayList<Integer>();
 		private ArrayList<Integer> gamesPlayedArr = new ArrayList<Integer>();
 		private ArrayList<Double> winMarginArr = new ArrayList<Double>(); // defined as ( pointDiff / gamesPlayed )
-		
+
 		public Football(String teamName, String division, int wins, int losses, int pointsScored, int pointsScoredOn) {
 			names.add(teamName);
 			divisions.add(division);
 			winsArr.add(wins);
 			lossesArr.add(losses);
 			pointsScoredArr.add(pointsScored);
+			pointsScoredOnArr.add(pointsScoredOn);
 			winLossPctArr.add((double) wins / losses);
 			pointDiffArr.add(pointsScored - pointsScoredOn);
 			gamesPlayedArr.add(wins + losses);
 			winMarginArr.add((double) pointDiffArr.get(names.indexOf(teamName) / gamesPlayedArr.get(names.indexOf(teamName))));
-			
+
+		}
+
+		public String getDivision(String teamName) {
+			return divisions.get(names.indexOf(teamName));
+		}
+
+		public int getWins(String teamName) {
+			return winsArr.get(names.indexOf(teamName));
+		}
+
+		public int getLosses(String teamName) {
+			return winsArr.get(names.indexOf(teamName));
+		}
+
+		public int getPointsScored(String teamName) {
+			return pointsScoredArr.get(names.indexOf(teamName));
+		}
+
+		public int getPointsScoredOn(String teamName) {
+			return pointsScoredOnArr.get(names.indexOf(teamName));
+		}
+
+		public double getWinLossPct(String teamName) {
+			return winLossPctArr.get(names.indexOf(teamName));
+		}
+
+		public int getPointDiff(String teamName) {
+			return pointDiffArr.get(names.indexOf(teamName));
+		}
+
+		public int getGamesPlayed(String teamName) {
+			return gamesPlayedArr.get(names.indexOf(teamName));
+		}
+
+		public double getWinMargin(String teamName) {
+			return winMarginArr.get(names.indexOf(teamName));
+		}
+
+		public String toString(String teamName) {
+			String team;
+			team = "Team: " + teamName + "\n Division: " + getDivision(teamName) + " Wins: " + getWins(teamName)
+					+ " Losses: " + getLosses(teamName) + " Points Scored: " + getPointsScored(teamName)
+					+ " Points Scored On: " + getPointsScoredOn(teamName) + " Wins/Losses Percent: "
+					+ getWinLossPct(teamName) + " Points Differential: " + getPointDiff(teamName) + " Games Played: "
+					+ getGamesPlayed(teamName) + " Win Margin: " + getWinMargin(teamName);
+			return team;
+		}
+	}
+
+	public static class Baseball extends SportTrack {
+		private ArrayList<String> names = new ArrayList<String>();
+		private ArrayList<Integer> winsArr = new ArrayList<Integer>();
+		private ArrayList<Integer> lossesArr = new ArrayList<Integer>();
+		private ArrayList<Integer> runsArr = new ArrayList<Integer>();
+		private ArrayList<Integer> homeRunsArr = new ArrayList<Integer>();
+		private ArrayList<Double> winLossPctArr = new ArrayList<Double>();
+		private ArrayList<Integer> hitsArr = new ArrayList<Integer>();
+		private ArrayList<Integer> gamesPlayedArr = new ArrayList<Integer>();
+		private ArrayList<Integer> rbiArr = new ArrayList<Integer>();
+		private ArrayList<Integer> totalRunsArr = new ArrayList<Integer>();
+		private ArrayList<Integer> basesStolenArr = new ArrayList<Integer>();
+
+		public Baseball(String teamName, int wins, int losses, int scoringRuns, int homeRuns, int hits, int rbi, int stolenBases, int totalRuns) {
+			names.add(teamName);
+			winsArr.add(wins);
+			lossesArr.add(losses);
+			runsArr.add(scoringRuns);
+			homeRunsArr.add(homeRuns);
+			hitsArr.add(hits);
+			winLossPctArr.add((double) wins / losses);
+			gamesPlayedArr.add(wins + losses);
+			rbiArr.add(rbi);
+			totalRunsArr.add(totalRuns);
+			basesStolenArr.add(stolenBases);
+
+		}
+
+		public int getWins(String teamName) {
+			return winsArr.get(names.indexOf(teamName));
+		}
+
+		public int getLosses(String teamName) {
+			return lossesArr.get(names.indexOf(teamName));
+		}
+
+		public int getScoringRuns(String teamName) {
+			return runsArr.get(names.indexOf(teamName));
+		}
+
+		public int getHomeRuns(String teamName) {
+			return homeRunsArr.get(names.indexOf(teamName));
+		}
+
+		public double getWinLossPct(String teamName) {
+			return winLossPctArr.get(names.indexOf(teamName));
+		}
+
+		public int getTotalHits(String teamName) {
+			return hitsArr.get(names.indexOf(teamName));
+		}
+
+		public int getGamesPlayed(String teamName) {
+			return gamesPlayedArr.get(names.indexOf(teamName));
+		}
+
+		public int getRbi(String teamName) {
+			return rbiArr.get(names.indexOf(teamName));
+		}
+
+		public int getTotalRuns(String teamName) {
+			return totalRunsArr.get(names.indexOf(teamName));
+		}
+
+		public int getBasesStolen(String teamName) {
+			return basesStolenArr.get(names.indexOf(teamName));
+		}
+
+		public String toString(String teamName) {
+			String team;
+			team = "Team: " + teamName + "\n Wins: " + getWins(teamName) + " Losses: " + getLosses(teamName)
+					+ " Points Scored: " + getScoringRuns(teamName) + " Home Runs: " + getHomeRuns(teamName)
+					+ " Wins/Losses Percent: " + getWinLossPct(teamName) + " Total Hits: " + getTotalHits(teamName)
+					+ " Games Played: " + getGamesPlayed(teamName) + " Runs Batted In: " + getRbi(teamName)
+					+ " Total Runs: " + getTotalRuns(teamName) + " Bases Stolen: " + getBasesStolen(teamName);
+			return team;
+		}
+	}
+
+	public static class Hockey extends SportTrack {
+		private ArrayList<String> names = new ArrayList<String>();
+		private ArrayList<String> divisions = new ArrayList<String>();
+		private ArrayList<String> subDivisions = new ArrayList<String>();
+		private ArrayList<Integer> winsArr = new ArrayList<Integer>();
+		private ArrayList<Integer> lossesArr = new ArrayList<Integer>();
+		private ArrayList<Integer> pointsScoredArr = new ArrayList<Integer>();
+		private ArrayList<Integer> pointsScoredOnArr = new ArrayList<Integer>();
+		private ArrayList<Double> winLossPctArr = new ArrayList<Double>();
+		private ArrayList<Integer> pointDiffArr = new ArrayList<Integer>();
+		private ArrayList<Integer> gamesPlayedArr = new ArrayList<Integer>();
+		private ArrayList<Double> winMarginArr = new ArrayList<Double>(); // defined as ( pointDiff / gamesPlayed )
+
+		public Hockey(String teamName, String division, String subDivision, int wins, int losses, int pointsScored, int pointsScoredOn) {
+			names.add(teamName);
+			divisions.add(division);
+			subDivisions.add(subDivision);
+			winsArr.add(wins);
+			lossesArr.add(losses);
+			pointsScoredArr.add(pointsScored);
+			pointsScoredOnArr.add(pointsScoredOn);
+			winLossPctArr.add((double) wins / losses);
+			pointDiffArr.add(pointsScored - pointsScoredOn);
+			gamesPlayedArr.add(wins + losses);
+			winMarginArr.add((double) pointDiffArr.get(names.indexOf(teamName) / gamesPlayedArr.get(names.indexOf(teamName))));
+
+		}
+
+		public String getDivision(String teamName) {
+			return divisions.get(names.indexOf(teamName));
 		}
 		
+		public String getSubDivision(String teamName) {
+			return subDivisions.get(names.indexOf(teamName));
+		}
+
+		public int getWins(String teamName) {
+			return winsArr.get(names.indexOf(teamName));
+		}
+
+		public int getLosses(String teamName) {
+			return winsArr.get(names.indexOf(teamName));
+		}
+
+		public int getPointsScored(String teamName) {
+			return pointsScoredArr.get(names.indexOf(teamName));
+		}
+
+		public int getPointsScoredOn(String teamName) {
+			return pointsScoredOnArr.get(names.indexOf(teamName));
+		}
+
+		public double getWinLossPct(String teamName) {
+			return winLossPctArr.get(names.indexOf(teamName));
+		}
+
+		public int getPointDiff(String teamName) {
+			return pointDiffArr.get(names.indexOf(teamName));
+		}
+
+		public int getGamesPlayed(String teamName) {
+			return gamesPlayedArr.get(names.indexOf(teamName));
+		}
+
+		public double getWinMargin(String teamName) {
+			return winMarginArr.get(names.indexOf(teamName));
+		}
+
+		public String toString(String teamName) {
+			String team;
+			team = "Team: " + teamName + "\n Division: " + getDivision(teamName) + " Sub-Division: " + getSubDivision(teamName) 
+					+ " Wins: " + getWins(teamName) + " Losses: " + getLosses(teamName) + " Points Scored: " 
+					+ getPointsScored(teamName) + " Points Scored On: " + getPointsScoredOn(teamName) + " Wins/Losses Percent: "
+					+ getWinLossPct(teamName) + " Points Differential: " + getPointDiff(teamName) + " Games Played: "
+					+ getGamesPlayed(teamName) + " Win Margin: " + getWinMargin(teamName);
+			return team;
+		}
+	}
+
+	public static class Soccer extends SportTrack {
+		private ArrayList<String> names = new ArrayList<String>();
+		private ArrayList<String> divisions = new ArrayList<String>();
+		private ArrayList<String> subDivisions = new ArrayList<String>();
+		private ArrayList<Integer> winsArr = new ArrayList<Integer>();
+		private ArrayList<Integer> lossesArr = new ArrayList<Integer>();
+		private ArrayList<Integer> pointsScoredArr = new ArrayList<Integer>();
+		private ArrayList<Integer> pointsScoredOnArr = new ArrayList<Integer>();
+		private ArrayList<Double> winLossPctArr = new ArrayList<Double>();
+		private ArrayList<Integer> pointDiffArr = new ArrayList<Integer>();
+		private ArrayList<Integer> gamesPlayedArr = new ArrayList<Integer>();
+		private ArrayList<Double> winMarginArr = new ArrayList<Double>(); // defined as ( pointDiff / gamesPlayed )
+
+		public Soccer(String teamName, String division, String subDivision, int wins, int losses, int pointsScored, int pointsScoredOn) {
+			names.add(teamName);
+			divisions.add(division);
+			subDivisions.add(subDivision);
+			winsArr.add(wins);
+			lossesArr.add(losses);
+			pointsScoredArr.add(pointsScored);
+			pointsScoredOnArr.add(pointsScoredOn);
+			winLossPctArr.add((double) wins / losses);
+			pointDiffArr.add(pointsScored - pointsScoredOn);
+			gamesPlayedArr.add(wins + losses);
+			winMarginArr.add((double) pointDiffArr.get(names.indexOf(teamName) / gamesPlayedArr.get(names.indexOf(teamName))));
+
+		}
+
+		public String getDivision(String teamName) {
+			return divisions.get(names.indexOf(teamName));
+		}
 		
-	}
-	
-	public class Baseball extends SportTrack {
-		public Baseball() {}
-	}
-	
-	public class Hockey extends SportTrack {
-		public Hockey() {}
-	}
-	
-	public class Soccer extends SportTrack {
-		public Soccer() {}
+		public String getSubDivision(String teamName) {
+			return subDivisions.get(names.indexOf(teamName));
+		}
+
+		public int getWins(String teamName) {
+			return winsArr.get(names.indexOf(teamName));
+		}
+
+		public int getLosses(String teamName) {
+			return winsArr.get(names.indexOf(teamName));
+		}
+
+		public int getPointsScored(String teamName) {
+			return pointsScoredArr.get(names.indexOf(teamName));
+		}
+
+		public int getPointsScoredOn(String teamName) {
+			return pointsScoredOnArr.get(names.indexOf(teamName));
+		}
+
+		public double getWinLossPct(String teamName) {
+			return winLossPctArr.get(names.indexOf(teamName));
+		}
+
+		public int getPointDiff(String teamName) {
+			return pointDiffArr.get(names.indexOf(teamName));
+		}
+
+		public int getGamesPlayed(String teamName) {
+			return gamesPlayedArr.get(names.indexOf(teamName));
+		}
+
+		public double getWinMargin(String teamName) {
+			return winMarginArr.get(names.indexOf(teamName));
+		}
+
+		public String toString(String teamName) {
+			String team;
+			team = "Team: " + teamName + "\n Division: " + getDivision(teamName) + " Wins: " 
+					+ getWins(teamName) + " Losses: " + getLosses(teamName) + " Points Scored: " 
+					+ getPointsScored(teamName) + " Points Scored On: " + getPointsScoredOn(teamName) + " Wins/Losses Percent: "
+					+ getWinLossPct(teamName) + " Points Differential: " + getPointDiff(teamName) + " Games Played: "
+					+ getGamesPlayed(teamName) + " Win Margin: " + getWinMargin(teamName);
+			return team;
+		}
 	}
 
 }
--- Workspace/Exercise7_4/src/exercise7_4.java
+++ Workspace/Exercise7_4/src/exercise7_4.java
@@ -6,23 +6,125 @@ import java.util.Scanner;
  */
 public class exercise7_4 {
 	private static Scanner sc = new Scanner(System.in);
-	private static int choice;
+	private static int choice, choice1;
+	private static SportTrack.Football team1; 
+	private static SportTrack.Baseball team2; 
+	private static SportTrack.Hockey team3; 
+	private static SportTrack.Soccer team4;
 	/**
 	 * @param args
+	 * @throws InterruptedException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
-		SportTrack main = new SportTracker();
 		
-		System.out.println("Enter 1 for a new football team, 2 for a new baseball team, 3 for a new hockey team, and 4 for a new soccer team. ");
+		System.out.println("Enter 1 for a new football team, 2 for a new baseball team, 3 for a new hockey team, and 4 for a new soccer team. \n");
 		choice = sc.nextInt();
 		
 		switch (choice) {
 		case 1:
-			String teamName, int wins, int losses, int pointsScored int pointsScoredOn;
-			System.out.println("Enter the team name");
-			SportTrack.Football player1 = new SportTrack.Football(teamName, int wins, int losses, int pointsScored, int pointsScoredOn);
-		
+			System.out.println("Football Chosen");
+			String teamName, division;
+			int wins, losses, pointsScored, pointsScoredOn;
+			System.out.println("Enter the team name: \n");
+			teamName = sc.nextLine();
+			System.out.println("Enter the division name: \n");
+			division = sc.nextLine();
+			System.out.println("Enter number of wins: \n");
+			wins = sc.nextInt();
+			System.out.println("Enter number of losses: \n");
+			losses = sc.nextInt();
+			System.out.println("Enter number of points scored: \n");
+			pointsScored = sc.nextInt();
+			System.out.println("Enter number of points scored on: \n");
+			pointsScoredOn = sc.nextInt();
+			team1 = new SportTrack.Football(teamName, division, wins, losses, pointsScored, pointsScoredOn);
+			System.out.println(team1.toString(teamName));
+			
+			
+		case 2:
+			System.out.println("Baseball Chosen");
+			String teamName1;
+			int wins1, losses1, scoringRuns, homeRuns, hits, rbi, stolenBases, totalRuns;
+			System.out.println("Enter team name: \n");
+			teamName1 = sc.nextLine();
+			System.out.println("Enter number of wins: \n");
+			wins1 = sc.nextInt();
+			System.out.println("Enter number of losses: \n");
+			losses1 = sc.nextInt();
+			System.out.println("Enter number of scoring runs: \n");
+			scoringRuns = sc.nextInt();
+			System.out.println("Enter number of home runs: \n");
+			homeRuns = sc.nextInt();
+			System.out.println("Enter number of hits: \n");
+			hits = sc.nextInt();
+			System.out.println("Enter number of runs batted in: \n");
+			rbi = sc.nextInt();
+			System.out.println("Enter number of bases stolen: \n");
+			stolenBases = sc.nextInt();
+			System.out.println("Enter number of total runs: \n");
+			totalRuns = sc.nextInt();
+			team2 = new SportTrack.Baseball(teamName1, wins1, losses1, scoringRuns, homeRuns, hits, rbi, stolenBases, totalRuns);
+			System.out.println(team1.toString(teamName1));
+		case 3:
+			System.out.println("Hockey Chosen");
+			String teamName2, division2, subDivision2;
+			int wins2, losses2, pointsScored2, pointsScoredOn2;
+			System.out.println("Enter the team name: \n");
+			teamName2 = sc.nextLine();
+			System.out.println("Enter the division name: \n");
+			division2 = sc.nextLine();
+			System.out.println("Enter the subdivision name: \n");
+			subDivision2 = sc.nextLine();
+			System.out.println("Enter number of wins: \n");
+			wins2 = sc.nextInt();
+			System.out.println("Enter number of losses: \n");
+			losses2 = sc.nextInt();
+			System.out.println("Enter number of points scored: \n");
+			pointsScored2 = sc.nextInt();
+			System.out.println("Enter number of points scored on: \n");
+			pointsScoredOn2 = sc.nextInt();
+			team3 = new SportTrack.Hockey(teamName2, subDivision2, division2, wins2, losses2, pointsScored2, pointsScoredOn2);
+			System.out.println(team3.toString(teamName2));
+		case 4:
+			System.out.println("Soccer Chosen");
+			String teamName3, division3, subDivision3;
+			int wins3, losses3, pointsScored3, pointsScoredOn3;
+			System.out.println("Enter the team name: \n");
+			teamName3 = sc.nextLine();
+			System.out.println("Enter the division name: \n");
+			division3 = sc.nextLine();
+			System.out.println("Enter the subdivision name: \n");
+			subDivision3 = sc.nextLine();
+			System.out.println("Enter number of wins: \n");
+			wins3 = sc.nextInt();
+			System.out.println("Enter number of losses: \n");
+			losses3 = sc.nextInt();
+			System.out.println("Enter number of points scored: \n");
+			pointsScored3 = sc.nextInt();
+			System.out.println("Enter number of points scored on: \n");
+			pointsScoredOn3 = sc.nextInt();
+			team4 = new SportTrack.Soccer(teamName3, subDivision3, division3, wins3, losses3, pointsScored3, pointsScoredOn3);
+			System.out.println(team4.toString(teamName3));
+		default:
+			System.out.println("Enter a team name: ");
+			String team5 = sc.nextLine();
+			System.out.println("Is this a football, baseball, hockey, or soccer team? (enter 1, 2, 3, or 4 respectively) \n");
+			choice1 = sc.nextInt();
+			switch(choice1) {
+			case 1:
+				System.out.println(team1.toString(team5));
+			case 2:
+				System.out.println(team2.toString(team5));
+			case 3:
+				System.out.println(team3.toString(team5));
+			case 4:
+				System.out.println(team4.toString(team5));
+			default:
+				System.out.println("Exiting...");
+				Thread.sleep(100);
+				System.exit(0);
+			}
 		}
 	}
 
